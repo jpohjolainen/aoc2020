7b17c98c06fe94b6b7f55d8975eb2bdec98e41e1
Janne Pohjolainen <janne.pohjolainen@polarsquad.com>
2020-12-06 11:18:55 +0100

    Added some comment

--- day6/aoc6_part2.py
+++ day6/aoc6_part2.py
@@ -8,6 +8,10 @@ def main():
         for line in f:
             line = line.rstrip()
             if not line:
+                ## Taking a set (set of string separates by character, so 'hey' => {'h','e','y'})
+                ## * unpacks the sets into individual blocks for set.interaction to only
+                ## match chars that are in all individual set, so f.ex [{'a'},{'ab'},{'abc'}] each
+                ## have 'a' common in all sets
                 sum += len(set.intersection(*[set(i) for i in comb]))
                 comb = []
                 continue
